f4400cace1e272dbf42fa2ad58e359a0bb5503da
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-03-08 13:17:37 +0100

    work on hero

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,19 +1,52 @@
-import { Link } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
 
 function MainHeader() {
     return (
-        <nav className="navbar custom-navbar">
-            <div className="container-fluid justify-content-center">
-                <Link to="/" className="text-decoration-none">
+        <header className="site-header">
+            <div className="site-header__inner">
+                <Link to="/" className="site-header__brand">
                     <img
                         src="/images/logoBoolZip.png"
                         alt="BoolZip"
                         className="header-logo"
                     />
                 </Link>
+
+                <nav className="site-header__nav">
+                    <NavLink to="/" className="site-header__link">
+                        Home
+                    </NavLink>
+                    <NavLink to="/products/1" className="site-header__link">
+                        Prodotti
+                    </NavLink>
+                    <NavLink to="/storia" className="site-header__link">
+                        Storia
+                    </NavLink>
+                </nav>
+
+                <div className="site-header__actions">
+                    <Link to="/cart" className="site-header__icon-btn" aria-label="Carrello">
+                        <i className="bi bi-cart4"></i>
+                    </Link>
+
+                    <Link
+                        to="/login"
+                        className="site-header__login site-header__login--desktop"
+                        aria-label="Login"
+                    >
+                        <i className="bi bi-person"></i>
+                    </Link>
+
+                    <button
+                        type="button"
+                        className="site-header__menu-toggle"
+                        aria-label="Apri menu"
+                    >
+                        <i className="bi bi-list"></i>
+                    </button>
+                </div>
             </div>
-        </nav>
+        </header>
     );
 }
 
